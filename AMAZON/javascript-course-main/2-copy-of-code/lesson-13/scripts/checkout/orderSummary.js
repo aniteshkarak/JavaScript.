8e70f1bc33ea3dd21d6bad3989e8f2c294c929ac
Anitesh Karak <209986326+aniteshkarak@users.noreply.github.com>
2026-06-19 12:53:31 +0530

    Refactor product rendering to utilize Product class methods for price and rating display

--- AMAZON/javascript-course-main/2-copy-of-code/lesson-13/scripts/checkout/orderSummary.js
+++ AMAZON/javascript-course-main/2-copy-of-code/lesson-13/scripts/checkout/orderSummary.js
@@ -48,7 +48,7 @@ export function renderOrderSummery (){
                         ${matchingProduct.name}
                     </div>
                     <div class="product-price">
-                        ${formatCurrency(matchingProduct.priceCents)}
+                        ${matchingProduct.getPrice()}
                     </div>
 
 
