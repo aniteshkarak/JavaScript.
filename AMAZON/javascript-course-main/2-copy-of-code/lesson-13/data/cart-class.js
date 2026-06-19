40c25031d9d38c8e64c04fa00581aaa707ccbc52
Anitesh Karak <209986326+aniteshkarak@users.noreply.github.com>
2026-06-19 12:18:47 +0530

    Add Cart class to manage cart items and local storage functionality

---
+++ AMAZON/javascript-course-main/2-copy-of-code/lesson-13/data/cart-class.js
@@ -0,0 +1,97 @@
+class Cart {
+    cartItems;
+    localstoragekey;
+
+    constructor(localstoragekey){
+        this.localstoragekey = localstoragekey;
+        this.loadFromStorage();
+    }
+
+    loadFromStorage() {
+        this.cartItems = JSON.parse(localStorage.getItem(this.localstoragekey));
+
+        if (!this.cartItems) {
+            this.cartItems = [
+                {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 2,
+                deliveryOptionId: '1'
+                },
+                {
+                productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                quantity: 1,
+                deliveryOptionId: '2'
+                }
+            ];
+        }
+    }
+    
+    saveToStorage() {
+        localStorage.setItem(this.localstoragekey, JSON.stringify(this.cartItems));
+    }
+
+    addToCart(productId) {
+        let matchingItem;
+
+        this.cartItems.forEach((item) => {
+            if (productId === item.productId) {
+            matchingItem = item;
+            }
+        });
+
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        } else {
+            this.cartItems.push({
+            productId: productId,
+            quantity: 1,
+            deliveryOptionId: '1'
+            });
+        }
+        this.saveToStorage();
+    }
+
+    removeFromCart(productId) {
+        const newCart = [];
+
+        this.cartItems.forEach((cartItem) => {
+            if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+            }
+        });
+
+        this.cartItems = newCart;
+
+        this.saveToStorage();
+    }
+
+    updateQuantity(productId, change) {
+        this.cartItems.forEach((item) => {
+            if (item.productId === productId) {
+            item.quantity += change;
+
+            if (item.quantity < 1) {
+                item.quantity = 1;
+            }
+            }
+        });
+
+        this.saveToStorage();
+    }
+
+    updateDeliveryOption(productId, deliveryOptionId) {
+        let matchingItem;
+
+        this.cartItems.forEach((item) => {
+            if (item.productId === productId) {
+            matchingItem = item;
+            }
+        });
+
+        matchingItem.deliveryOptionId = deliveryOptionId;
+        this.saveToStorage();
+    }
+}
+
+
+    const cart = new Cart('cart-oop');
--- AMAZON/javascript-course-main/2-copy-of-code/lesson-13/scripts/checkout.js
+++ AMAZON/javascript-course-main/2-copy-of-code/lesson-13/scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummery } from './checkout/orderSummary.js';
 import { renderPaymentSummery } from './checkout/paymentSummary.js';
-import '../data/cart_oops.js';
+import '../data/cart-class.js';
 
 renderOrderSummery();
 renderPaymentSummery();
