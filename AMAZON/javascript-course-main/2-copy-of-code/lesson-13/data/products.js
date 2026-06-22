5970341a578731678adbeada362cc734d995dc18
Anitesh Karak <209986326+aniteshkarak@users.noreply.github.com>
2026-06-22 16:19:18 +0530

    Add loadCart function and refactor checkout process to load products and cart data

--- AMAZON/javascript-course-main/2-copy-of-code/lesson-13/data/products.js
+++ AMAZON/javascript-course-main/2-copy-of-code/lesson-13/data/products.js
@@ -85,9 +85,9 @@ export function loadProducts(fun){
       return new Product(productDetails);
     });
 
-    console.log('load products');
+    // console.log('load products');
     fun();
-  });
+  }); 
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
--- AMAZON/javascript-course-main/2-copy-of-code/lesson-13/scripts/checkout.js
+++ AMAZON/javascript-course-main/2-copy-of-code/lesson-13/scripts/checkout.js
@@ -1,11 +1,51 @@
 import { renderOrderSummery } from './checkout/orderSummary.js';
 import { renderPaymentSummery } from './checkout/paymentSummary.js';
+import { loadCart } from '../data/cart.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
 import { loadProducts } from '../data/products.js';
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart (() => {
+            resolve();
+        });
+    })
+]).then(() => {
     renderOrderSummery();
-    renderPaymentSummery();
+    renderPaymentSummery(); 
 });
+
+/*
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('value1');
+    });
+
+}).then((value) => {
+    return new Promise((resolve) => {
+        loadCart (() => {
+            resolve();
+        });
+    });
+
+}).then(() => {
+    renderOrderSummery();
+    renderPaymentSummery(); 
+});
+*/
+
+/*
+loadProducts(() => {
+    loadCart(() => {
+        renderOrderSummery();
+        renderPaymentSummery();
+    });
+});
+*/
