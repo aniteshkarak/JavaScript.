d51483f8fc7b75df94fd73d289506fa4ad8ca7b7
Anitesh Karak <209986326+aniteshkarak@users.noreply.github.com>
2026-06-23 09:20:15 +0530

    Add loadProductsFetch function to fetch and load products from backend

--- AMAZON/javascript-course-main/2-copy-of-code/lesson-13/data/products.js
+++ AMAZON/javascript-course-main/2-copy-of-code/lesson-13/data/products.js
@@ -74,6 +74,29 @@ object3.method();
 */
 export let products = [];
 
+export function loadProductsFetch(fun){
+  fetch('https://supersimplebackend.dev/products').then( (response) =>{
+    return response.json();
+  }).then( (productData) => {
+    products = productData.map((productDetails) => {
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    // console.log('load products');
+    fun();
+  });
+  return Promise;
+}
+
+/*
+loadProductsFetch().then(() => {
+  console.log('fwsauif');
+});
+*/
+
 export function loadProducts(fun){
   const xhr = new XMLHttpRequest();
   
