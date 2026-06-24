35bcb96f7a00ade2affeb9843a8ed9051fd5df65
Anitesh Karak <209986326+aniteshkarak@users.noreply.github.com>
2026-06-24 17:14:10 +0530

    create order using backend

--- AMAZON/javascript-course-main/2-copy-of-code/lesson-13/scripts/checkout/paymentSummary.js
+++ AMAZON/javascript-course-main/2-copy-of-code/lesson-13/scripts/checkout/paymentSummary.js
@@ -2,6 +2,8 @@ import {cart} from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import { getDeliveryOption } from '../../data/deliveryOptions.js';
 import { formatCurrency } from '../utils/money.js';
+import { addOrder } from '../../data/orders.js';
+
 
 export function renderPaymentSummery (){
     let productPriceCents = 0;
@@ -59,7 +61,7 @@ export function renderPaymentSummery (){
             </div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary js-place-order">
             Place your order
         </button>
     `;
@@ -75,4 +77,25 @@ export function renderPaymentSummery (){
     document.querySelector('.js-total-items').innerHTML = `Items (${totalItems}):`;
 
     document.querySelector('.js-return-to-home-link').innerHTML = `${totalItems} items`;
+
+    document.querySelector('.js-place-order').addEventListener('click', async () => {
+        try {
+            const response = await fetch ('https://supersimplebackend.dev/orders', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    cart: cart
+                })
+            });
+            const order = response.json();
+            addOrder(order);
+
+        } catch (error){
+            console.log('Unexpected error. Try again later.');
+        }
+        
+        window.location.href = 'orders.html';
+    });
 }
