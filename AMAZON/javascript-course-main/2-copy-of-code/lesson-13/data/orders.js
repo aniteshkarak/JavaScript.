f19600c317e100a5c495e603c2fe8daaefbfd780
Anitesh Karak <209986326+aniteshkarak@users.noreply.github.com>
2026-06-25 09:41:15 +0530

    Implement order tracking feature and update orders display

--- AMAZON/javascript-course-main/2-copy-of-code/lesson-13/data/orders.js
+++ AMAZON/javascript-course-main/2-copy-of-code/lesson-13/data/orders.js
@@ -6,5 +6,29 @@ export function addOrder(order){
 }
 
 function saveToStorage(){
-    localStorage.setItem('order', JSON.stringify(orders));
+    localStorage.setItem('orders', JSON.stringify(orders));
 }
+
+import { orders } from '../data/orders.js';
+import { getProduct } from '../data/products.js';
+
+let ordersHTML = '';
+
+orders.forEach((order) => {
+  order.products.forEach((productDetails) => {
+
+    const product = getProduct(productDetails.productId);
+
+    ordersHTML += `
+      <div class="product-actions">
+        <a href="tracking.html?productId=${product.id}">
+          <button class="track-package-button button-secondary">
+            Track Package
+          </button>
+        </a>
+      </div>
+    `;
+  });
+});
+
+document.querySelector('.orders-grid').innerHTML = ordersHTML;
